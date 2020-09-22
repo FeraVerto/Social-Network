b0bff5223a1f826144e0d4c3881e5cfbf81bcd14
FeraVerto <themightymasha@gmail.com>
2020-09-22 13:52:18 +0500

    Updates

---
+++ precache-manifest.8e037237dcac2c4d024fbd2e715a38c6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30345f7b7d5c5935c0740fc623287d25",
+    "url": "/Social-Network/index.html"
+  },
+  {
+    "revision": "cbe08c36591f4f2cc58d",
+    "url": "/Social-Network/static/css/3.2393a667.chunk.css"
+  },
+  {
+    "revision": "4c490fc72bfa946e7b4e",
+    "url": "/Social-Network/static/css/4.c27a1b7e.chunk.css"
+  },
+  {
+    "revision": "f0d754c6dc11948023c5",
+    "url": "/Social-Network/static/css/main.7c728177.chunk.css"
+  },
+  {
+    "revision": "6183966ab0b1ff320343",
+    "url": "/Social-Network/static/js/2.fef5f93f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Social-Network/static/js/2.fef5f93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe08c36591f4f2cc58d",
+    "url": "/Social-Network/static/js/3.50b61857.chunk.js"
+  },
+  {
+    "revision": "4c490fc72bfa946e7b4e",
+    "url": "/Social-Network/static/js/4.4a5a4272.chunk.js"
+  },
+  {
+    "revision": "f0d754c6dc11948023c5",
+    "url": "/Social-Network/static/js/main.a500043e.chunk.js"
+  },
+  {
+    "revision": "fb9573f282bfe5d07e36",
+    "url": "/Social-Network/static/js/runtime-main.2bccbf2c.js"
+  },
+  {
+    "revision": "46cc5c93f4849ebf1d7f6346fe008021",
+    "url": "/Social-Network/static/media/base-avatar.46cc5c93.png"
+  },
+  {
+    "revision": "3a658c63fdacd093ef78f05aeade5886",
+    "url": "/Social-Network/static/media/preloader.3a658c63.svg"
+  }
+]);
